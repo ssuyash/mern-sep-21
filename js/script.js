060bd9845bf3a5a01f3d06a41d650789c2651b3b
ssuyash <kumarsuyash94@gmail.com>
2021-10-19 14:21:40 +0530

    js started variables and datatypes

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var a = 10
+var b = 0b100
+var c = 0O147
+var d = 0xffa
+
+var e = 1/0
+var f = 10.5
+
+console.log(a, b, c, d, e, f)
+
+
+
+var isTimeOver = false
+var ableToUnderstand = true
+
+var name = "geek\nsdoor"
+var trainer = 'suyash\'s'
+var courseContent = `html
+CSS
+js`
+
+
+var user = "Vaibhav"
+var greetStr = `Hello ${user}  Goood Afternoon`
+
+var arr = [10, 20, true, undefined, "abc"]
+
+console.log(arr[0], arr[1])
+
+var person = {
+    name:'suyash',
+    age:27,
+    profession:['enterprenuer', "Tech Coach"]
+}
+
+console.log(person['profession'])
+
+// console.log(greetStr)
+
+
+
+
