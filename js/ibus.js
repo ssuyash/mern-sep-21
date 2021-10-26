fd9b2b9dd8ecbdbe1857c56dd340b4ad261a4820
ssuyash <kumarsuyash94@gmail.com>
2021-10-26 14:33:40 +0530

    ibus problem

--- js/ibus.js
+++ js/ibus.js
@@ -7,20 +7,59 @@ var stations = {
 	},
 
 	VPS:{
-		dist:1,
+		dist:5,
 		next:"BHW",
 		prev:"RGS"
 	},
 
+    BHW:{
+		dist:11,
+		next:"HLK",
+		prev:"VPS"
+	},
 
+    HLK:{
+		dist:17,
+		next:"NVL",
+		prev:"BHW"
+	},
+
+    NVL:{
+		dist:19,
+		next:"",
+		prev:"HLK"
+	},
 }
 
-var src = "RGS"
-var dst = "VPS"
+var src = "HLK"
+var dst =  "VPS"
 
 var srcInfo = stations[src]
 var dstInfo = stations[dst]
 
-var srcRltDst = srcInfo.dist
+var isContigeous = srcInfo.next == dst || srcInfo.prev == dst
 
-console.log(srcInfo, dstInfo)
+var travelDist = dstInfo.dist - srcInfo.dist
+
+travelDist = travelDist < 0 ? -1*travelDist : travelDist
+
+var fare = 0
+
+if(isContigeous){
+    fare = 5
+}else{
+    fare = 5
+    var additionalDst = travelDist-3
+
+    if(additionalDst > 0){
+        fare += 1.5*additionalDst
+    }
+}
+console.log(src, dst, isContigeous, travelDist, fare)
+
+// var course = {
+//     courseName:"mern stack",
+//     couurseContent:[],
+    
+// }
+// var cn = "courseName"    
