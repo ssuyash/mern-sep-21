4efd6cb9cfef61de4f22cd414dbe99239f127635
ssuyash <kumarsuyash94@gmail.com>
2021-10-27 14:43:07 +0530

    loop

---
+++ js/for-loop.js
@@ -0,0 +1,23 @@
+// var n = 50
+// for(var count=1; count<=10; count++){
+//   console.log(n*count)
+// }
+
+
+var n = 17
+
+var count = 0
+
+for(var i=1; i<=n; i++){
+    if(n % i == 0){
+        console.log(i)
+        count++
+    }
+}
+console.log("total number : " , count)
+
+if(count > 2){
+    console.log("not prime")
+}else{
+    console.log("prime")
+}
---
+++ js/switch-case.js
@@ -0,0 +1,33 @@
+var day = 7
+
+switch (day) {
+    case 1: console.log("Monday")
+        break
+    case 2: console.log("Tuesday")
+        break
+    case 3: console.log("Wed")
+        break
+    case 4: console.log("Thus")
+        break
+    case 5: console.log("Fri")
+        break
+    case 6: console.log("Sat")
+        break
+    case 7: console.log("Sun")
+        break
+    default:
+        console.log("invalid day count")
+
+}
+
+var word = ""
+switch (word) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case "o":
+    case "u": console.log("vowel")
+    break
+    default:
+        console.log("consonent")
+}
